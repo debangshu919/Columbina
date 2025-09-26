8ad01da3686888262f7346dc8a34ae2c15a70b21
Debangshu <personal.debangshu444@gmail.com>
2025-09-26 19:36:07 +0530

    refactor: turned off database logs

--- src/services/database_service.py
+++ src/services/database_service.py
@@ -2,6 +2,6 @@ from sqlmodel import SQLModel, create_engine
 
 from configs import env
 
-engine = create_engine(env.DATABASE_URL, echo=True)  # turn off echo in prod.
+engine = create_engine(env.DATABASE_URL)  # turn off echo in prod.
 
 SQLModel.metadata.create_all(engine)
